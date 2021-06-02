c3cda304e04eafcbc80979bb1d30519469631e28
lazy-void <salnikov.k54@gmail.com>
2021-06-02 20:34:22 +0300

    Rename command directory

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ ARCH=("386" "amd64" "arm" "arm64")
 OS=("linux" "darwin" "windows")
 for arch in "${ARCH[@]}"; do
   for os in "${OS[@]}"; do
-    BIN_PATH="primitive_bot"
-    ARCHIVE_PATH="${BIN_PATH}_${os}_${arch}.tar.gz"
+    BIN_PATH="primitive-bot"
+    ARCHIVE_PATH="primitive_bot_${os}_${arch}.tar.gz"
     if [[ "$os" == "windows" ]]; then
       BIN_PATH="$BIN_PATH.exe"
     fi
 
-    env GOARCH="$arch" GOOS="$os" go build -o "$BIN_PATH" ../cmd/bot/*.go
+    env GOARCH="$arch" GOOS="$os" go build -o "$BIN_PATH" ../cmd/primitive-bot/*.go
     if [ $? -eq 0 ]; then
       tar cfz "$ARCHIVE_PATH" "$BIN_PATH"
       rm "$BIN_PATH"
